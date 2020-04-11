c73e9f689d647f4cc80447ed2c1c40bb3cb6f1ef
kumankit <33811029+kumankit@users.noreply.github.com>
2020-04-11 10:58:19 +0530

    Updates

---
+++ precache-manifest.7053e15104c0ce4be2dbaeda77945d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a02a0c133a32b5f3f6dfd1da5437783",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "e93131120526d39f31c6",
+    "url": "/covid19/static/css/main.6de8985d.chunk.css"
+  },
+  {
+    "revision": "de6a5c11365c9e3239cf",
+    "url": "/covid19/static/js/2.978d4a37.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.978d4a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93131120526d39f31c6",
+    "url": "/covid19/static/js/main.d57e663b.chunk.js"
+  },
+  {
+    "revision": "cdfcea41dfab553d9d0f",
+    "url": "/covid19/static/js/runtime-main.9f6017dd.js"
+  }
+]);
